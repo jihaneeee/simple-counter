de844be083243d8c8e0ced65a5b367a7c1a5873f
jihane <nijadijihane@gmail.com>
2024-11-23 13:55:05 +0100

    add dynamic color changes

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ const increaseElement = document.getElementById("increase");
 const decreaseElement = document.getElementById("decrease");
 const resetElement = document.getElementById("reset");
 
+function updateColor(){
+    if (numberElement.innerHTML > 0 ){
+        numberElement.style.color = "green";
+    } else if (numberElement.innerHTML < 0 ){
+        numberElement.style.color = "red";
+    } else {
+        numberElement.style.color = "black";
+    }
+}
 
-
-
- increaseElement.addEventListener("click",function(){
-   
+increaseElement.addEventListener("click",function(){
     numberElement.innerHTML++;
- });
- decreaseElement.addEventListener("click",function(){
-   
+    updateColor();
+});
+
+decreaseElement.addEventListener("click",function(){
     numberElement.innerHTML--;
- });
- resetElement.addEventListener("click",function(){
-    
+    updateColor();
+});
+ 
+resetElement.addEventListener("click",function(){
     numberElement.innerHTML = 0;
- });
+    updateColor();
+});
